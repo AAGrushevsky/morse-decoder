a0dafc2ff61d0233df0f42d98d1bb878d9dee193
AAGrushevsky <111446919+AAGrushevsky@users.noreply.github.com>
2023-08-08 21:58:51 +0300

    docs: Update index.js

--- src/index.js
+++ src/index.js
@@ -37,8 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function fragmentation(str, part) {
+  let pattern = new RegExp(".{1," + part + "}", "ig");
+  return str.match(pattern).map(item => item.padEnd(part, "."))
+}
+
+function crop(sequence) {
+  let morseKey = sequence.replace(/^0+/, '');
+  return morseKey;
+}
+
+function convert(str) {
+  return str === '10' ? '.' : '-';
+}
+
 function decode(expr) {
-    // write your solution here
+  // write your solution here
+  let arrCodeWords = expr.split('**********');
+  let arrCodeWordsLettersLong = arrCodeWords.map(item => fragmentation(item, 10));
+  let arrCodeWordsLettersShort = arrCodeWordsLettersLong.map(item => item.map(el => crop(el)));
+  let arrKeyWordsLetters = arrCodeWordsLettersShort.map(item => item.map(el => fragmentation(el, 2).map(part => convert(part)).join('')).map(el => MORSE_TABLE[el]).join('')).join(' ')
+  return arrKeyWordsLetters;
 }
 
 module.exports = {
